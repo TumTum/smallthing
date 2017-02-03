df3ff2355faf720a122f7c56a502b5e22eedaa2e
Tobias Matthaiou <developer@tobimat.de>
2017-02-03 08:48:07 +0100

    Eine weiche eingebaut um die Formular Daten richtig weiter zuleiten.

---
+++ src/ControllerWeiche.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Class ControllerWeiche
+ *
+ * wenn Formulare abgeschossen werden, damit sie richtig um geleitet werden.
+ */
+class ControllerWeiche
+{
+
+    /**
+     * @var WebSession
+     */
+    protected $webSession;
+
+    public function __construct($webSession)
+    {
+        $this->webSession = $webSession;
+    }
+
+    public function starten()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['controller_form'])) {
+            $this->weiche();
+        }
+    }
+
+    /**
+     * Die Weiche
+     */
+    protected function weiche()
+    {
+        $wunsch_controller = $_POST['controller_form'];
+        switch ($wunsch_controller) {
+
+        }
+    }
+}
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,3 +5,6 @@ include_once "autoloader.php";
 // Bei jedem Starten der Webseite ist es wichtig die Session zu starten
 $webSession = new WebSession();
 $webSession->starten();
+
+$controllerWeiche = new ControllerWeiche($webSession);
+$controllerWeiche->starten();
