0c30a6d63dd5e68fdcc950be68aea1d33439e752
Tobias Matthaiou <developer@tobimat.de>
2017-02-03 09:15:26 +0100

    Erste prüfung ob das Formular überhaupt richtig ausgefüllt ist.

--- src/Controller/MitarbeiterPage.php
+++ src/Controller/MitarbeiterPage.php
@@ -5,6 +5,12 @@ namespace Controller;
 
 class MitarbeiterPage
 {
+
+    /**
+     * @var array
+     */
+    protected static $errors = [];
+
     /**
      * Gibt an ob der User eingelogt ist oder nicht
      *
@@ -22,14 +28,43 @@ class MitarbeiterPage
      */
     public function hasError()
     {
+        if (count(self::$errors) > 0) {
+            return true;
+        }
         return false;
     }
 
     /**
-     * @param $webSession
+     * @param \WebSession $webSession
      */
     public function login($webSession)
     {
-        
+        if ($this->UserUeberpruefen()) {
+            // Login bestanden
+        }
     }
+
+    /**
+     * Prüft die Formular daten, wenn sie Okay sind ist der User eingeloggt
+     *
+     * @return bool
+     */
+    public function UserUeberpruefen()
+    {
+        // Prüft ob es Fromular einträge gibt
+        if (isset($_POST['email']) == false || empty($_POST['email'])) {
+            self::$errors[] = 'Fehler keine Email Adresse vorhanden';
+        }
+        if (isset($_POST['password']) == false || empty($_POST['password'])) {
+            self::$errors[] = 'Fehler keine Passwort vorhanden';
+        }
+
+        // Wenn es fehler gab, dann ist diese User Überprüfung fehl geschlagen.
+        if ($this->hasError()) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
