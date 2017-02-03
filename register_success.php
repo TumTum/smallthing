27f190eeb22439a03be0bfe97dbf341e306912bb
Tobias Matthaiou <developer@tobimat.de>
2017-02-03 07:25:40 +0100

    Letzte HTML Seiten repariert.

--- register_success.php
+++ register_success.php
@@ -1,49 +1,45 @@
- <ul class="topnav" id="myTopnav">
-  <li><a href="/index.php">Home</a></li>
-  <li><a href="/speisekarte.php">Speisekarte</a></li>
-  <li><a href="/bestellung.php">Bestellung</a></li>
-  <li><a href="/kontakt.php">Kontakt</a></li>
-</ul>
-
-
-<style type="text/css">
-ul.topnav {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-    /* Float the list items side by side */
-    
-}
-/* Float the list items side by side */
-ul.topnav li {float: left;}
-
-ul.topnav li a {
-    display: inline-block;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    transition: 0.3s;
-    font-size: 17px;
-}
-/* Change background color of links on hover */
-ul.topnav li a:hover {background-color: #555;}
-
-/* Hide the list item that contains the link that should open and close the topnav on small screens */
-ul.topnav li.icon {display: none;}
-
-</style>
-
-<!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8"><br><br><br>
-        <title>Registration Erfolgreich!</title>
+        <title>Fastfood MacAPPLE - Registration Erfolgreich!</title>
         <link rel="stylesheet" href="styles/main.css" />
+
+        <style type="text/css">
+            ul.topnav {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+                /* Float the list items side by side */
+
+            }
+            /* Float the list items side by side */
+            ul.topnav li {float: left;}
+
+            ul.topnav li a {
+                display: inline-block;
+                color: #f2f2f2;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                transition: 0.3s;
+                font-size: 17px;
+            }
+            /* Change background color of links on hover */
+            ul.topnav li a:hover {background-color: #555;}
+
+            /* Hide the list item that contains the link that should open and close the topnav on small screens */
+            ul.topnav li.icon {display: none;}
+
+        </style>
     </head>
     <body>
+        <ul class="topnav" id="myTopnav">
+            <li><a href="/index.php">Home</a></li>
+            <li><a href="/speisekarte.php">Speisekarte</a></li>
+            <li><a href="/bestellung.php">Bestellung</a></li>
+            <li><a href="/kontakt.php">Kontakt</a></li>
+        </ul>
         <h1>Registration Erfolgreich!</h1>
         <p>Du kannst nun zurück zur <a href="login.php">Loginfenster</a> und dich Einloggen.</p>
     </body>
